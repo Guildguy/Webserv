a7b9ff5d29c6275730393d4d6c94a7f79e1df521
Fábio_Luiz <fabiolljunior23@gmail.com>
2026-01-28 19:54:03 +0000

    refactor(server): convert socket creation and configuration to member functions

--- server.cpp
+++ server.cpp
@@ -1,40 +1,51 @@
 #include "server.hpp"
 
-int  create_n_config_socket(Server &server)
+void  Server::createAndConfigServersocket()
 {
-  server.fd = socket(AF_INET, SOCK_STREAM, 0);
-  //ERR TREATMENT
-  if (server.fd < 0)
+  this->fd = socket(AF_INET, SOCK_STREAM, 0);
+  validateServerSocketCreation();
+
+  configServerNonBlocking();
+  configServerReuseAddress();
+}
+
+void Server::validateServerSocketCreation()
+{
+  if (this->fd < 0)
   {
     perror("failed to create a server socket!");
-    close(server.fd);
     exit(1);
   }
   std::cout << "server socket created!" << std::endl;
+}
 
-  //SEPARAR POSTERIORMENTE
-  int flag = fcntl(server.fd, F_GETFL, 0);
+void Server::configServerNonBlocking()
+{
+  int flag = fcntl(this->fd, F_GETFL, 0);
+
   if (flag == -1)
   {
     perror("error: fcntl(F_GETFL)");
-    close(server.fd);
+    close(this->fd);
     exit(1);
   }
 
-  if (fcntl(server.fd, F_SETFL, flag | O_NONBLOCK) == -1)
+  if (fcntl(this->fd, F_SETFL, flag | O_NONBLOCK) == -1)
   {
     perror("error: fcntl(F_SETFL)");
-    close(server.fd);
+    close(this->fd);
     exit(1);
   }
+}
 
-  //SEPARAR POSTERIORMENTE
+void Server::configServerReuseAddress()
+{
   int opt = 0;
 
-  if (setsockopt(server.fd, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt)) < 0)
+  if (setsockopt(this->fd, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt)) < 0)
   {
     std::cerr << "setsockopt err\n" << std::endl;
-    close(server.fd);
+    close(this->fd);
     exit(1);
   }
 }
