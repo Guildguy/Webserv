04f74b5bd2467fa85d02881ce8fadb8b406a6b4c
Fabio <fabiolljunior23@gmail.com>
2026-02-11 16:44:42 -0300

    feat(server): implement FileDescriptor class with constructors and destructor

---
+++ server/fileDescriptor.cpp
@@ -0,0 +1,9 @@
+#include "includes/fileDescriptor.hpp"
+
+FileDescriptor::FileDescriptor() : _fd(-1) {}
+
+FileDescriptor::FileDescriptor(int fd) : _fd(-1) {}
+
+~FileDescriptor::FileDescriptor() {}
+
+
