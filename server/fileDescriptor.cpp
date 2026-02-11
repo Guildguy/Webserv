17089f483434cc081409341c3d062d911290cf5e
Fabio <fabiolljunior23@gmail.com>
2026-02-11 16:56:14 -0300

    fix(fileDescriptor): add setReuseAddress methods for socket options management

--- server/fileDescriptor.cpp
+++ server/fileDescriptor.cpp
@@ -33,12 +33,25 @@ bool FileDescriptor::setNonBlocking()
     return (FileDescriptor::setNonBlocking(_fd));
 }
 
+bool FileDescriptor::setReuseAddress()
+{
+    return (FileDescriptor::setReuseAddress(_fd));
+}
+
 bool FileDescriptor::setNonBlocking(int fd)
 {
     int flag = fcntl(fd, F_GETFL, 0);
     if (flag == -1)
         return (false);
     if (fcntl(fd, F_SETFL, flag | O_NONBLOCK) == -1)
+        return (false);
+    return (true);
+}
+
+bool FileDescriptor::setReuseAddress(int fd)
+{
+    int opt = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
         return (false);
     return (true);
 }
