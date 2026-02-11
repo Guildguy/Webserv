6a994cdc5cfc0900a7ce59418809119fff503980
Fabio <fabiolljunior23@gmail.com>
2026-02-11 20:26:54 -0300

    feat(ServerSocket): implement createSocket method for socket creation and configuration

--- server/serverSocket.cpp
+++ server/serverSocket.cpp
@@ -3,3 +3,20 @@
 ServerSocket::ServerSocket() {}
 
 ServerSocket::~ServerSocket() {}
+
+bool    ServerSocket::createSocket()
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    _fd = FileDescriptor(fd);
+
+    if (!_fd.isValid())
+        return (handleError("error: socket creation failed"));
+    if (!_fd.setNonBlocking())
+        return (handleError("error: setNonBlocking failed"));
+    
+    int opt = 1;
+    if (setsockopt(_fd.get(), SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+        return (handleError("error: setsockopt failed"));
+
+    return (true);
+}
