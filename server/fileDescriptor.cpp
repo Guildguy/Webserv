4cc73d359071f80980fdc58659c330a3d6fcba5f
Fabio <fabiolljunior23@gmail.com>
2026-02-11 16:54:57 -0300

    fix(fileDescriptor): correct constructor initialization and enhance destructor functionality

--- server/fileDescriptor.cpp
+++ server/fileDescriptor.cpp
@@ -2,8 +2,28 @@
 
 FileDescriptor::FileDescriptor() : _fd(-1) {}
 
-FileDescriptor::FileDescriptor(int fd) : _fd(-1) {}
+FileDescriptor::FileDescriptor(int fd) : _fd(fd) {}
 
-~FileDescriptor::FileDescriptor() {}
+FileDescriptor::~FileDescriptor()
+{
+    invalidate();
+}
 
+bool FileDescriptor::isValid() const
+{
+    return (_fd >= 0);
+}
 
+int FileDescriptor::getValue() const
+{
+    return (_fd);
+}
+
+void FileDescriptor::invalidate()
+{
+    if (_fd != -1)
+    {
+        close(_fd);
+        _fd = -1;
+    }
+}
