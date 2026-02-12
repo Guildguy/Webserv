9eb414e0c6d79eccafc500a04fc262a1b7e24863
Fabio <fabiolljunior23@gmail.com>
2026-02-12 20:26:21 -0300

    feat(ClientSocket): implement receiveData and sendData methods

--- server/clientSocket.cpp
+++ server/clientSocket.cpp
@@ -28,3 +28,12 @@ int     ClientSocket::getPollFd() const
     return (_fd.getPollFd);
 }
 
+ssize_t ClientSocket::receiveData(char* buffer, size_t size)
+{
+
+}
+
+ssize_t ClientSocket::sendData(const char* data, size_t size)
+{
+    
+}
