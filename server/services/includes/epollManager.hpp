c128b0905762ca111090b68bbb4490e78915b623
Fabio Junior <fabiolljunior23@gmail.com>
2026-03-01 15:39:31 -0300

    feat(includes): added new includes, also, new class members with incapsulation

--- server/services/includes/epollManager.hpp
+++ server/services/includes/epollManager.hpp
@@ -5,23 +5,26 @@
 #include <unistd.h>
 #include <vector>
 #include <cstring>
+#include <stdexcept>
+#include "../../domain/includes/epollEvents.hpp"
 
 class	EpollManager
 {
 	private:
-		int					_epollFd;
-		struct epoll_event	_triggeredEvents[1024];
-		int					_readyEventsCount;
+		int							_epollFd;
+		MaxEvents					_maxEvents;
+		std::vector<epoll_event>	_triggeredEvents;
+		int							_readyEventsCount;
 
 	public:
-		EpollManager();
+		explicit EpollManager(const MaxEvents& maxEvents);
 		~EpollManager();
 		
 		void	addFd(int fd, short event);
 		void	removeFd(int fd);
 		int		waitForEvents();
 		
-		int getEventFd(int index) const;
+		int		getEventFd(int index) const;
 };
 
 #endif
